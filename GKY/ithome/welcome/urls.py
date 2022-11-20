6131a987dbb54e5e42c7c77d1be5b1161f14c917
soulcat69 <58879713+soulcat69@users.noreply.github.com>
2022-11-20 12:08:54 +0800

    new some dick
    new some dick

--- GKY/ithome/welcome/urls.py
+++ GKY/ithome/welcome/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.index),
+    path('product/', views.product),
+    path('detail/<int:id>',views.detail, name = 'detail-url')
 ]
