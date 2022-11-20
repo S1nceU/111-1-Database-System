284505ec66645d3d6d0a470b7b583790cfb1b72f
soulcat69 <58879713+soulcat69@users.noreply.github.com>
2022-11-20 12:14:59 +0800

    adjust admin page with admin.modelAdmin

--- GKY/ithome/welcome/admin.py
+++ GKY/ithome/welcome/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from . import models
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pmodel', 'nickname', 'price', 'year')
+
+
 # Register your models here.
 admin.site.register(models.Maker)
 admin.site.register(models.PModel)
-admin.site.register(models.Product)
+admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.PPhoto)
