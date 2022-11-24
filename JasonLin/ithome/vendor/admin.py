8c6e0eaa6a4763b40a9ba4afc28df7f452338e82
JasonLin9123 <t109590001@ntut.org.tw>
2022-11-25 02:13:54 +0800

    User API 製作完成

--- JasonLin/ithome/vendor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Vendor, Food, VendorAdmin
-
-# Register your models here.
-# admin.site.register(Vendor, VendorAdmin)
-# admin.site.register(Food)
