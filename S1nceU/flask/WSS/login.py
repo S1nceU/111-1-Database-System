f9564e7c83d8b8910c250c8215c2c5911e156562
S1nceU <jjkk900925@gmail.com>
2022-12-06 22:04:50 +0800

    customer_login

--- S1nceU/flask/WSS/login.py
+++ S1nceU/flask/WSS/login.py
@@ -6,22 +6,50 @@ import mysql_unit
 login = Blueprint('login', __name__, template_folder='templates')
 
 @login.route('/login/', methods=['GET', 'POST'])
-def login_test():
+def login_seller():
     db = mysql_unit.connect()
     if request.method == 'POST':
         # req_account = request.form.get("account")
         # req_password = request.form.get("password")
         req_account  = request.json['account']
         req_password = request.json['password']
-        data = mysql_unit.login_comfirm(db,"*","seller",req_account)
+        data,user_level = mysql_unit.login_comfirm(db,"*","seller",req_account)
         if data is None:
-            print("test",data,"a",req_account,"p",req_password)
+            # print("test",data,"a",req_account,"p",req_password)
+            print("No account."+"account = "+ req_account + " password = "+ req_password)
             return '0'
+        elif req_password != data['password']:
+            print("Password is wrong."+"account = "+ req_account + " password = "+ req_password)
+            return '1'
+        elif user_level:
+            print(data['username'],", Hello admin auth")
+            return '3'
         else:
-            if req_password != data['password']:
-                return '1'
-            else:
-                print('Hello ' + data['username'])
-                return '2'
+            print('Hello ' + data['username'])
+            return '2'
+    db.close()
+
+@login.route('/login_c/', methods=['GET', 'POST'])
+def login_seller():
+    db = mysql_unit.connect()
+    if request.method == 'POST':
+        # req_account = request.form.get("account")
+        # req_password = request.form.get("password")
+        req_account  = request.json['account']
+        req_password = request.json['password']
+        data,user_level = mysql_unit.login_comfirm(db,"*","customer",req_account)
+        if data is None:
+            # print("test",data,"a",req_account,"p",req_password)
+            print("No account."+"account = "+ req_account + " password = "+ req_password)
+            return '0'
+        elif req_password != data['password']:
+            print("Password is wrong."+"account = "+ req_account + " password = "+ req_password)
+            return '1'
+        elif user_level:
+            print(data['username'],", Hello admin auth")
+            return '3'
+        else:
+            print('Hello ' + data['username'])
+            return '2'
     db.close()
         
--- S1nceU/flask/WSS/mysql_unit.py
+++ S1nceU/flask/WSS/mysql_unit.py
@@ -5,25 +5,45 @@ def connect():
     return db
 
 def login_comfirm(db,SELECT,FROM,WHERE):
-    condition = (SELECT,FROM,WHERE)
+    condition = (FROM,FROM,FROM,FROM,WHERE)
+
 
     sql_cmd = """
-            select %s
+            (select %s.username,%s.account,%s.password
             from %s
-            where account = '%s'
+            where account = '%s' )
             """%condition
+    sql_cmd_a = """
+            (select admin.username,admin.account,admin.password
+            from admin
+            where admin.account = '%s')
+            """%WHERE
     account = db.cursor()
     account.execute(sql_cmd)
     data = account.fetchone()
+    account.execute(sql_cmd_a)
+    data_admin = account.fetchone()
+    user_level = 0
     if data is None:
-        return data
-
+        if data_admin is None:
+            return data
+        else:
+            user_level = 1
+        
     # if len(data) == 0:
-    data = {
-        'username' : data[1],
-        'account'  : data[2],
-        'password' : data[3]
-    }
-    return data
+    if user_level == 0:
+        data = {
+            'username' : data[0],
+            'account'  : data[1],
+            'password' : data[2]
+        }
+    else:
+        data = {
+            'username' : data_admin[0],
+            'account'  : data_admin[1],
+            'password' : data_admin[2]
+        }
+    print(type(data))
+    return data,user_level
 def disconnect(db):
     db.close()
