4925ccaf070fbd29fdf5de11b1c95ec8b2b3f86e
ygdelta <t109590015@ntut.org.tw>
2022-12-29 00:40:58 +0800

    新增回報功能

---
+++ S1nceU/flask/WSS/static/src/report.js
@@ -0,0 +1,78 @@
+import {createApp} from 'vue'
+
+const selectBar = createApp({
+    data() {
+        return {
+            username: 'username',
+            logged: false,
+            accountLevel: ''
+        }
+    },
+    methods: {
+        async getData() {
+            let res = await axios.post("http://127.0.0.1:5000/isLogined/", {})
+            let loginData = await res.data
+            if(loginData == 'False') {
+                this.logged = false
+                return
+            }
+            this.username = await loginData.username
+            this.accountLevel = await loginData.user_level
+            this.logged = true
+        },
+        Logout() {
+            Cookies.remove("WSS")
+            this.logged = false
+            this.goHome()
+        },
+        goLogin() {
+            window.location.replace("http://127.0.0.1:5000/login")
+        },
+        goHome() {
+            window.location.replace("http://127.0.0.1:5000/home")
+        },
+        goSeller() {
+            window.location.replace("http://127.0.0.1:5000/seller")
+        },
+        goOrder() {
+            window.location.replace("http://127.0.0.1:5000/order")
+        },
+        goCart() {
+            window.location.replace("http://127.0.0.1:5000/cart")
+        },
+        goMember() {
+            window.location.replace("http://127.0.0.1:5000/member")
+        },
+        goRegister() {
+            window.location.replace("http://127.0.0.1:5000/register")
+        },
+        goAdmin() {
+            window.location.replace("http://127.0.0.1:5000/admin")
+        }
+    },
+    computed:{
+        welcome() {
+            return "歡迎!" + this.username
+        }
+    },
+    created() {
+        this.getData()
+    }
+}).mount(".tt")
+
+const report = createApp({
+    data() {
+        return {
+            reportText: ""
+        }
+    },
+    methods: {
+        async Report() {
+            let res = await axios.post("http://127.0.0.1:5000/report", {content: this.reportText})
+            if(res.data == "Report success.") {
+                alert("回報成功!")
+                window.location.reload()
+            }
+        }
+    }
+}).mount('.content')
